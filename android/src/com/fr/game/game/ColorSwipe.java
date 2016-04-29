ebdd4b01a2383d9828de1f6f0962853fff8cfd4c
Syed Saad Hussain <syhussai@adobe.com>
2016-04-29 15:47:35 +0530

    First Commit

---
+++ android/src/com/fr/game/game/ColorSwipe.java
@@ -0,0 +1,16 @@
+package com.fr.game.game;
+
+import com.badlogic.gdx.Game;
+
+/**
+ * Created by syhussai on 4/29/2016.
+ *
+ * Entry Point Of The Game
+ */
+public class ColorSwipe extends Game{
+
+    @Override
+    public void create() {
+
+    }
+}
